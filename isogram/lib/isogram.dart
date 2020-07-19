5f1ec2249f8100c3b044a05d5e3e92333c75d550
Doo-Dong <ehd6754@gmail.com>
2020-07-19 15:33:11 +0900

    'Isogram' code passed all tests.

--- isogram/lib/isogram.dart
+++ isogram/lib/isogram.dart
@@ -1,5 +1,10 @@
 class Isogram {
-  bool isIsogram(String string) {
-    //code
-  }
+  bool isIsogram(String string) =>
+      removeRedundancy(dataPreprocess(string)) == dataPreprocess(string);
+
+  String dataPreprocess(String string) =>
+      string.replaceAll(' ', '').replaceAll('-', '').toUpperCase();
+
+  String removeRedundancy(String string) =>
+      string.split('').toSet().toList().join('');
 }
