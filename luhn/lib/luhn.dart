3effe19f9a6bed3ff97be345cf3233078db37180
Doo-Dong <ehd6754@gmail.com>
2020-07-20 22:24:08 +0900

    commit Incomplete Solutions

--- luhn/lib/luhn.dart
+++ luhn/lib/luhn.dart
@@ -1,5 +1,26 @@
 class Luhn {
   bool valid(String string) {
-    //code
+    List<int> integer = string
+        .replaceAll(new RegExp(r'(\D+)'), '')
+        .split('')
+        .map(int.parse)
+        .toList();
+    int integerLength = integer.join('').toString().length;
+
+    for (int i = integerLength; i < 0; i - 2) {
+      if (integer[i] * 2 > 9) {
+        integer[i] = (integer[i] * 2)
+            .toString()
+            .split('')
+            .map(int.parse)
+            .map((e) => e)
+            .fold(0, (a, b) => a + b);
+      } else {
+        integer[i] = integer[i] * 2;
+      }
+    }
+    print(integer);
+
+    return (integer.fold<int>(0, (a, b) => a + b) % 10 == 0);
   }
 }
